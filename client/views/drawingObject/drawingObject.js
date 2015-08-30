db5c980a27aab4e095b703943a1f5777cd78981f
us <u@ulf.codes>
2015-08-30 11:51:13 +0200

    make dragging more smooth and much more efficient by only persisting the move on drag stop. the behavior is no similar to the resizing behavior.

--- client/views/drawingObject/drawingObject.js
+++ client/views/drawingObject/drawingObject.js
@@ -219,7 +219,7 @@ Meteor.drawingObject = {
                         if (event.pageY + this.height > e.height()) {
                             e.height(e.height() + 100);
                         }
-                        Meteor.drawingObject.updatePosition(this._id, false); //intentionally not changing z-index
+                        Meteor.drawingObject.updatePosition(this._id, false); //intentionally not changing z-index and not persisting
                         //TODO maybe persist the position update after drag delay? Anyway to only persist on dragstop is much more efficient for the database
                     }
                 },
