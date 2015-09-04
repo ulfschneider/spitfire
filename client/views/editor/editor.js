70ec03025131b6a041d34e0a9aa1bb6bf1bb384d
us <u@ulf.codes>
2015-09-04 17:29:32 +0200

    add 200 px drawing width and height when maintaining the boundaries

--- client/views/editor/editor.js
+++ client/views/editor/editor.js
@@ -32,8 +32,8 @@ Meteor.editor = {
 
         var e = $('#editor');
         var w = $(window);
-        e.width(Math.max(w.width(), Math.max(e.width(), Meteor.canvas.drawingWidth())));
-        e.height(Math.max(w.height(), Math.max(e.height(), Meteor.canvas.drawingHeight())));
+        e.width(Math.max(w.width(), Math.max(e.width(), Meteor.canvas.drawingWidth() + 200)));
+        e.height(Math.max(w.height(), Math.max(e.height(), Meteor.canvas.drawingHeight() + 200)));
     },
     init: function () {
         $(window).on('scroll', function () {
