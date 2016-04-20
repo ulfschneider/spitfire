42ac14fe1b7066af726057c5e1d3c36ebbb60daf
ulfschneider <u@ulf.codes>
2016-04-20 20:06:38 +0200

    Remove auth parameter for URL

--- client/views/auth/auth.js
+++ client/views/auth/auth.js
@@ -1,14 +1,6 @@
 "use strict";
 
-var AUTH_PARAM_ID = ",auth";
-
 Meteor.auth = {
-    parseAuth: function (url) {
-        if (url) {
-            return url.toLowerCase().indexOf(AUTH_PARAM_ID) >= 0;
-        }
-        return false;
-    },
     setAuth: function (auth) {
         if (Meteor.auth.isAuth() !== (true === auth)) {
             Session.set("auth", auth === true);
@@ -16,14 +8,7 @@ Meteor.auth = {
     },
     isAuth: function () {
         return Meteor.user() || true === Session.get("auth");
-    },
-    getAuthString: function () {
-        if (Meteor.auth.isAuth()) {
-            return AUTH_PARAM_ID;
-        }
-        return "";
     }
-
 };
 
 
