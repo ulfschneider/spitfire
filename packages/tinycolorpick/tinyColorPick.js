cf54ac4eb637154aea94e6c34936deb4ce8231e0
ulfschneider <u@ulf.codes>
2016-06-12 15:22:44 +0200

    Back to less colors

--- packages/tinycolorpick/tinyColorPick.js
+++ packages/tinycolorpick/tinyColorPick.js
@@ -1,6 +1,6 @@
 var tinyColorPickOptions = {
     opacity: .6,
-    colors: ['#ffffff', '#f1453d', '#e62565', '#9b2fae', '#673fb4', '#4054b2', '#2b98f0', '#1eaaf1', '#1fbcd2', '#159588', '#50ae55', '#8cc152', '#cdda49', '#fee94e', '#fdc02f', '#fd9727', '#fc5830', '#785549', '#9e9e9e', '#617d88', '#fafad2'],
+    colors: ['#ffffff', '#999999', '#000000', '#66ff66', '#006600', '#ff9900', '#cc0000', '#99ccff', '#3399ff', '#000066', '#fafa33', '#fafad2'],
     colorsPerLine: 3
 };
 
